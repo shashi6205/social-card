3fe2e1e15ce3e79653791672aac911645760be08
Shashi Kumar <shashi.h.kumar@oracle.com>
2020-12-31 11:52:06 +0530

    Create Basic React App

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SocialCard from './SocialCard';
+
+const App = () => {
+  return <div className="App container text-center mt-4">
+    <SocialCard />
+  </div>
+}
+
+export default App;
---
+++ src/components/SocialCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialCard = () => {
+    return (
+        <div className="SocialCard mb-3 row">
+            <h1>Social Card</h1>
+        </div>
+    );
+};
+
+export default SocialCard;
